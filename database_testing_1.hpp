3f4fa9bfe0f1ff2e93adc8a3807a15e9f96dbaa7
PrathameshJadhav33 <prathameshjadhav136@gmail.com>
2021-06-04 10:35:57 +0530

    header file for database testing

---
+++ database_testing_1.hpp
@@ -0,0 +1,10 @@
+#ifndef database_testing_1_CPP_INCLUDED
+#define database_testing_1_CPP_INCLUDED
+// ^^^ these are header guards, used to avoid multiple inclusions of the same header
+
+// Declarations
+
+// Notice that this does not define a global variable, it just says to the compiler "in some module there's a global named in this way, let me access it"
+void db_test();
+
+#endif
