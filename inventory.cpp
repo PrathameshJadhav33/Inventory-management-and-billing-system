0af37b4f6851983ed73ac52b47d02070b245c10f
PrathameshJadhav33 <prathameshjadhav136@gmail.com>
2021-06-06 13:38:39 +0530

    Basic functionality added to perform each task

--- inventory.cpp
+++ inventory.cpp
@@ -27,7 +27,8 @@ private:
 
     stringstream ss;
 
-    int stay;
+    int stay=0;
+    char opt;
 
     sqlite3_stmt* stmt;
 public:
@@ -35,6 +36,7 @@ public:
     {
         data="\n\t callback invoked";
         zErrMsg =0;
+        stay=0;
         //open database or create if does not exist
         rc = sqlite3_open("/D:/KIT/Semester 4/mini project/inventory.db", &db1);
 
@@ -83,7 +85,7 @@ public:
         system("cls");
         inv_display();
         open_db();
-        cout << "\n\t\t\t\t1. Product id:\t";
+        cout << "\n\n\t\t\t\t1. Product id:\t";
         cin >> id;
         cout << "\n\t\t\t\t2. Product Name:\t";
         cin >> name;
@@ -94,66 +96,209 @@ public:
         cout << "\n\t\t\t\t5. Total Quantity:\t";
         cin >> qty;
         cout << "\n\n\t\t\t\t Add product to database?(y/n):\t";
-        cin >> choice;
+        cin >> opt;
 
-        //sql2 = "INSERT INTO STOCK VALUES ("+id1+","+name+","+category+","+price1+","+qty1+");";
-        sql2 = "INSERT INTO STOCK VALUES (?,?,?,?,?);";
-        sqlite3_prepare_v2(db1,sql2.c_str(),sql2.length(),&stmt, NULL);
+            if(opt=='y'){
+
+                sql2 = "INSERT INTO STOCK VALUES (?,?,?,?,?);";
+                rc=sqlite3_prepare_v2(db1,sql2.c_str(),sql2.length(),&stmt, NULL);
+                sqlite3_bind_int(stmt,1,id);
+                sqlite3_bind_text(stmt,2,name.c_str(),name.length(),NULL);
+                sqlite3_bind_text(stmt,3,category.c_str(),category.length(),NULL);
+                sqlite3_bind_int(stmt,4,price);
+                sqlite3_bind_int(stmt,5,qty);
+
+                sqlite3_step(stmt);
+                sqlite3_finalize(stmt);
+
+                    if (rc != SQLITE_OK)
+                    {
+                        cout << "\n\t\t\t\tunable to INSERT RECORD";
+                        cout << "\n\t\t\t\tError:" << zErrMsg;
+                        sqlite3_free(zErrMsg);
+                    }
+                    else
+                    {
+                        cout << "\n\t\t\t\tProduct added to database successfully";
+                    }
+            }
+
+        cout<<"\n\t\t\t\tEnter 1 to exit : ";
+            while(stay!=1){
+                 cin >> stay;
+            }
+        sqlite3_close(db1);
+    }
+
+    void update_product(){
+
+        system("cls");
+        inv_display();
+        open_db();
+
+        cout << "\n\n\t\t\t\tEnter product id:";
+        cin >> id;
+
+        cout << "\n\t\t\t\tEnter product new Quantity:";
+        cin >> qty;
+
+        cout << "\n\n\t\t\t\tUpdate product?(y/n):\t";
+        cin >> opt;
+
+        if(opt=='y'){
+
+            sql2 = "UPDATE STOCK set QTY = ? where ID = ?; SELECT * from STOCK";
+            rc=sqlite3_prepare_v2(db1,sql2.c_str(),sql2.length(),&stmt, NULL);
+            sqlite3_bind_int(stmt,1,qty);
+            sqlite3_bind_int(stmt,2,id);
+
+            sqlite3_step(stmt);
+            sqlite3_finalize(stmt);
+
+                    if (rc != SQLITE_OK)
+                    {
+                        cout << "\n\t\t\t\tunable to Update product";
+                        cout << "\n\t\t\t\tError:" << zErrMsg;
+                        sqlite3_free(zErrMsg);
+                    }
+                    else
+                    {
+                        cout << "\n\t\t\t\tProduct updated successfully";
+                    }
+        }
+
+        cout<<"\n\t\t\t\tEnter 1 to exit : ";
+        while(stay!=1){
+             cin >> stay;
+        }
+        sqlite3_close(db1);
+    }
+
+    void del_product(){
+        system("cls");
+        inv_display();
+        open_db();
+
+        cout << "\n\n\t\t\t\tEnter product id:";
+        cin >> id;
+
+        cout << "\n\n\t\t\t\tUpdate product?(y/n):\t";
+        cin >> opt;
+
+        if(opt=='y'){
+
+        sql2 = "DELETE from STOCK where ID = ?; SELECT * from STOCK";
+        rc=sqlite3_prepare_v2(db1,sql2.c_str(),sql2.length(),&stmt, NULL);
         sqlite3_bind_int(stmt,1,id);
-        sqlite3_bind_text(stmt,2,name.c_str(),name.length(),NULL);
-        sqlite3_bind_text(stmt,3,category.c_str(),category.length(),NULL);
-        sqlite3_bind_int(stmt,4,price);
-        sqlite3_bind_int(stmt,5,qty);
 
         sqlite3_step(stmt);
         sqlite3_finalize(stmt);
 
-        //rc = sqlite3_exec(db1, stmt, callback, (void *)data, &zErrMsg);
-        if (rc != SQLITE_OK)
-        {
-            //cout << "\n\tunable to INSERT RECORD";
-            cout << "\n\tError:" << zErrMsg;
-            sqlite3_free(zErrMsg);
-        }
-        /*
-        else
-        {
-            cout << "\n\t\t RECORD created successfully";
-        }*/
+            if (rc != SQLITE_OK)
+            {
+                cout << "\n\t\t\t\tunable to delete product";
+                cout << "\n\t\t\t\tError:" << zErrMsg;
+                sqlite3_free(zErrMsg);
+            }
+            else
+            {
+                cout << "\n\t\t\t\tProduct deleted successfully";
+            }
 
-        cin >> stay;
+        }
+
+        cout<<"\n\t\t\t\tEnter 1 to exit : ";
+        while(stay!=1){
+             cin >> stay;
+        }
         sqlite3_close(db1);
+
     }
 
      void select()
     {
-        //sql select query
+        system("cls");
+        inv_display();
+
         open_db();
+
         sql = "SELECT * FROM STOCK";
+        cout <<"\n";
+        cout <<"\n\n\t\tID\t\tName\t\tCategory\tPrice\t\tQuantity";
+
         rc = sqlite3_exec(db1, sql, callback, (void *)data, &zErrMsg);
-        if (rc != SQLITE_OK)
-        {
-            //cout << "\n\tunable to FETCH RECORD";
-            cout << "\n\tError:" << zErrMsg;
-            sqlite3_free(zErrMsg);
-        }
-        else
-        {
-            cout << "\n\t\t RECORDS fetched successfully";
-        }
-        cin >> stay;
-        sqlite3_close(db1);
+            if (rc != SQLITE_OK)
+            {
+                cout << "\n\t\t\t\tunable to FETCH RECORD";
+                cout << "\n\t\t\t\tError:" << zErrMsg;
+                sqlite3_free(zErrMsg);
+            }
+        cout<<"\n\t\t\t\tEnter 1 to exit : ";
+            while(stay!=1){
+                 cin >> stay;
+            }
+            sqlite3_close(db1);
     }
 
+    void ruuning_out_of_stock(){
+
+        system("cls");
+        inv_display();
+
+        open_db();
+
+        sql = "SELECT * FROM STOCK where QTY BETWEEN 1 AND 20";
+        cout <<"\n";
+        cout <<"\n\n\t\tID\t\tName\t\tCategory\tPrice\t\tQuantity";
+
+        rc = sqlite3_exec(db1, sql, callback, (void *)data, &zErrMsg);
+            if (rc != SQLITE_OK)
+            {
+                cout << "\n\t\t\t\tunable to FETCH RECORD";
+                cout << "\n\t\t\t\tError:" << zErrMsg;
+                sqlite3_free(zErrMsg);
+            }
+        cout<<"\n\t\t\t\tEnter 1 to exit : ";
+            while(stay!=1){
+                 cin >> stay;
+            }
+            sqlite3_close(db1);
+
+
+    }
+
+    void out_of_stock(){
+
+        system("cls");
+        inv_display();
+
+        open_db();
+
+        sql = "SELECT * FROM STOCK where QTY == 0";
+        cout <<"\n";
+        cout <<"\n\n\t\tID\t\tName\t\tCategory\tPrice\t\tQuantity";
+
+        rc = sqlite3_exec(db1, sql, callback, (void *)data, &zErrMsg);
+            if (rc != SQLITE_OK)
+            {
+                cout << "\n\t\t\t\tunable to FETCH RECORD";
+                cout << "\n\t\t\t\tError:" << zErrMsg;
+                sqlite3_free(zErrMsg);
+            }
+        cout<<"\n\t\t\t\tEnter 1 to exit : ";
+            while(stay!=1){
+                 cin >> stay;
+            }
+            sqlite3_close(db1);
+
+
+    }
+
+
+    //this function display the records row by row
      static int callback(void *data, int col_in_row, char **fields_in_row, char **Col_names)
     {
-        //cout << (const char *)data;
         cout <<"\n";
-        for (int i = 0; i < col_in_row; i++)
-        {
-            cout << "\t\t" << Col_names[i];
-        }
-        cout << "\n";
 
         for (int i = 0; i < col_in_row; i++)
         {
@@ -195,25 +340,23 @@ void inventory()
         switch (choice)
         {
         case 1:
-            cout << "case 1";
             obj.add_product();
             break;
         case 2:
-            cout << "case 2";
-            db_test();
+            obj.update_product();
             break;
         case 3:
-            cout << "case 1";
+            obj.del_product();
             break;
         case 4:
-            cout << "case 2";
             obj.select();
+
             break;
         case 5:
-            cout << "case 1";
+            obj.ruuning_out_of_stock();
             break;
         case 6:
-            cout << "case 2";
+            obj.out_of_stock();
             break;
         case 7:
             break;
